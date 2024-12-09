00132dded20f597678599f6254f9affbfd630555
Bartłomiej Kruczek <plgkruczek@login01.helios.cyfronet.pl>
2024-12-09 10:42:01 +0100

    not working yet

--- pipeline.py
+++ pipeline.py
@@ -1,6 +1,7 @@
 import torch
 import time
 import json
+import numpy as np
 
 from utils import Utils
 from data import Data
@@ -77,12 +78,20 @@ class MyPipeline:
         for elem, subfolder_name, json_ground_path in dataset:
             yield elem, subfolder_name, json_ground_path
 
-    def train(self, model=None, optimizer=None, criterion=None, num_epochs: int = 1, debug: bool = True) -> None:
+    def train(self, 
+              model = None, 
+              optimizer = None, 
+              criterion = None, 
+              num_epochs: int = 1, 
+              do_generate_json_during_training: bool = True,
+              do_dump_text: bool = False,
+              debug: bool = True) -> None:
+        
         self.my_utils.delete_past_jsons()
         separate_outputs = []
         
-        self.model_to_train.train()
-        optimizer = AdamW(params = self.model_to_train, lr = 1e-5)
+        model_train = self.model_to_train.train()
+        optimizer = AdamW(params = model_train.parameters(), lr = 1e-5)
         criterion = CrossEntropyLoss
 
         # adding special tokens to tokenizer, only once before training
@@ -96,86 +105,117 @@ class MyPipeline:
         print(f"Added {num_added_tokens} special tokens.")
 
         self.model_qwen2.resize_token_embeddings(len(tokenizer))
-        self.model_qwen2half.resize_token_embeddings(len(tokenizer_2))
+        self.model_qwen2half.resize_token_embeddings(len(tokenizer_2), mean_resizing = False)
 
         for epoch in range(1, num_epochs + 1):
             print(f"Starting epoch {epoch}")
             start_time = time.time()
             elem_counter = 0
 
-            for elem, subfolder_name, json_ground_path in tqdm(self.dataset_generator(), desc="Processing dataset"):
+            for elem, subfolder_name, json_ground_path in tqdm(self.dataset_generator(), desc = "Processing dataset"):
                 try:
                     elem_counter += 1
-                    print(f"Processing element {elem_counter}", flush=True)
+                    print("\n ----------------------", flush = True)
+                    print(f"Processing element {elem_counter} \n", flush = True)
 
                     # Process inputs
                     text = self.my_qwen2.processor.apply_chat_template(
-                        elem, tokenize=False, add_generation_prompt=True
+                        elem, 
+                        tokenize = False, 
+                        add_generation_prompt = True
                     )
                     image_inputs, video_inputs = process_vision_info(elem)
                     inputs = self.my_qwen2.processor(
-                        text=text,
-                        images=image_inputs,
-                        videos=video_inputs,
-                        padding=True,
-                        return_tensors="pt",
+                        text = text,
+                        images = image_inputs,
+                        videos = video_inputs,
+                        padding = True,
+                        return_tensors = "pt",
                     ).to("cuda")
-                    
 
-                    # Optimized token generation
-                    generated_ids = self.my_qwen2.model.generate(
-                        **inputs,
-                        max_new_tokens=2048,  # Limit the number of generated tokens
-                        do_sample=True,  # Sampling for faster generation
-                        temperature=0.0,  # No randomness
-                        use_cache=True  # Enable caching for faster computation
-                    )
-                    generated_ids_trimmed = [
-                        out_ids[len(in_ids):] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
-                    ]
-                    output_text = self.my_qwen2.processor.batch_decode(
-                        generated_ids_trimmed,
-                        skip_special_tokens=True,
-                        clean_up_tokenization_spaces=False
-                    )
+                    if debug:
+                        print(f"Inputs: {inputs}", flush = True)
 
-                    # Process Qwen2.5
-                    dumped_text = self.my_qwen2half.make_json_from_generated_text(
-                        generated_text=output_text,
-                        subfolder_name=subfolder_name,
-                        json_path=json_ground_path,
-                    )
+                    if do_generate_json_during_training:
+                        # Optimized token generation
+                        generated_ids = self.my_qwen2.model.generate(
+                            **inputs,
+                            max_new_tokens = 2048,  # Limit the number of generated tokens
+                            do_sample = False,  # Sampling for faster generation
+                            temperature = 0.01,  # No randomness
+                            use_cache = True  # Enable caching for faster computation
+                        )
+                        generated_ids_trimmed = [
+                            out_ids[len(in_ids):] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+                        ]
+                        output_text = self.my_qwen2.processor.batch_decode(
+                            generated_ids_trimmed,
+                            skip_special_tokens=True,
+                            clean_up_tokenization_spaces=False
+                        )
 
-                    # Calculate metrics
-                    # calculated_TED = self.my_metrics.calculate_tree_edit_distance(
-                    #     json_generated=dumped_text,
-                    #     json_test_path=json_ground_path
-                    # )
-                    # print(f"Calculated TED: {calculated_TED}", flush=True)
+                        if debug:
+                            print(f"Output text: {output_text}")
+
+                        # Process Qwen2.5
+                        if do_dump_text:
+                            dumped_text = self.my_qwen2half.make_json_from_generated_text(
+                                generated_text=output_text,
+                                subfolder_name=subfolder_name,
+                                json_path=json_ground_path,
+                            )
+
+                            # Calculate metrics
+                            # calculated_TED = self.my_metrics.calculate_tree_edit_distance(
+                            #     json_generated=dumped_text,
+                            #     json_test_path=json_ground_path
+                            # )
+                            # print(f"Calculated TED: {calculated_TED}", flush=True)
 
                     # dumped text is combined json from one law document, here we convert it to tokens as well as ground truth json
 
                     # open ground truth json
-                    with open(json_ground_path, "r", encoding="utf-8") as f:
-                        json_obj = json.load(f)
+                    if debug:
+                        print(f"Processing {json_ground_path} file \n", flush = True)
 
-                    truth_tokens: list[str] = self.my_json.json_to_token_conversion(json_obj = json_obj)
+                    try:
+                        string1: str = ""
+                        with open(json_ground_path, 'r', encoding = 'utf-8') as file1:
+                            string1: str = file1.read()
+
+                        json1: dict = json.loads(string1)
+
+                        if debug:
+                            print(f"Success in loading json object: {json1}", flush = True)
+                    except Exception as e:
+                        print(f"Error occured: {e}")
+
+                    truth_tokens: list[str] = self.my_json.json_to_token_conversion(json_obj = json1, keys_only = False)
                     
-                    # load dumped json
-                    json_generated = json.load(dumped_text)
-                    generated_tokens: list[str] = self.my_json.json_to_token_conversion(json_obj = json_generated)
+                    if do_generate_json_during_training and do_dump_text:
+                        # load dumped json
+                        json_generated = json.load(dumped_text)
+                        generated_tokens: list[str] = self.my_json.json_to_token_conversion(json_obj = json_generated)
 
                     if debug:
-                        print(f"Json truth tokens: {truth_tokens}")
-                        print(f"Json generated tokens: {generated_tokens}")
+                        print(f"Json truth tokens: {truth_tokens}", flush = True)
+
+                        if do_generate_json_during_training and do_dump_text:
+                            print(f"Json generated tokens: {generated_tokens}", flush = True)
 
                     labels = tokenizer(
                         truth_tokens,
                         is_split_into_words = True,
-                        return_tensors = "pt"
+                        return_tensors = "pt",
+                        padding = True,
+                        truncation = True,
                     ).input_ids.to("cuda")
 
-                    outputs = self.model_qwen2(**inputs, labels = labels)
+                    if debug:
+                        print(f"Shape inputs: {inputs.input_ids.shape}")
+                        print(f"Shape labels: {labels.shape}")
+
+                    outputs = model_train(**inputs, labels = labels)
                     loss = outputs.loss
 
                     loss.backward()
@@ -184,7 +224,7 @@ class MyPipeline:
 
                     print(f"Loss: {loss.item()}", flush = True)
 
-                    separate_outputs.append((output_text, subfolder_name))
+                    # separate_outputs.append((output_text, subfolder_name))
 
                 except Exception as e:
                     print(f"Error processing element {elem_counter}: {e}", flush=True)
