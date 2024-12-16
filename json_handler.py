8a2cd56bb7dda4e89b2789936ee2e756458132c2
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2024-12-16 09:48:08 +0100

    changes

--- json_handler.py
+++ json_handler.py
@@ -158,3 +158,47 @@ class JsonHandler:
             print(f"Error occured {str(e)} in function {self.get_special_tokens_json_ground_truth.__name__}")
     
         return list(all_uniqe_tokens)
+    
+    def create_tree_from_tokens(self, tokens: list[str], start_index: int = 0, label: str = "root") -> None:
+        """
+        Here is function to create custom nodes from tokens structure.
+        """
+        my_node = CustomNodeCreator(label = label)
+        i = start_index
+
+        while i < len(tokens):
+            token = tokens[i]
+
+            # Sprawdź, czy to zamykający znacznik klucza lub listy
+            if token.startswith("</x_") and token.endswith(">"):
+                # Koniec aktualnego bloku obiektu
+                return my_node, i + 1
+
+            elif token == "</list>":
+                # Koniec aktualnej listy
+                return my_node, i + 1
+
+            # Sprawdź, czy to otwierający znacznik klucza obiektu
+            elif token.startswith("<x_") and token.endswith(">"):
+                # Wyciągamy nazwę klucza
+                key_name = token[3:-1]  # usuń '<x_' i '>'
+                # Rekurencyjnie budujemy poddrzewo dla tego klucza
+                child_node, new_index = self.create_tree_from_tokens(tokens, i + 1, label=key_name)
+                my_node.add_children(child_node)
+                i = new_index
+
+            # Sprawdź, czy to początek listy
+            elif token == "<list>":
+                # Rekurencja dla listy
+                child_node, new_index = self.create_tree_from_tokens(tokens, i + 1, label="list")
+                my_node.add_children(child_node)
+                i = new_index
+
+            else:
+                # Tu powinny trafiać wartości prymitywne (jeśli keys_only=False)
+                # lub pomniejsze elementy nie będące strukturą (liście)
+                value_node = CustomNodeCreator(label=token)
+                my_node.add_children(value_node)
+                i += 1
+
+        return my_node, i
